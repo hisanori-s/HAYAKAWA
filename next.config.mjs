800e3072cfdb9be7584ad5fbc37d4f929978ef70
satokupo <satokupo>
2024-12-21 12:57:19 +0900

    本番環境のスクエアにつなげる

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,10 @@ import { join } from 'path';
 
 // 環境変数ファイルの自動切り替え
 // デフォルトでサンドボックス環境を使用
-copyFileSync(join(process.cwd(), 'env', '.env.local.sandbox'), join(process.cwd(), '.env.local'));
+// copyFileSync(join(process.cwd(), 'env', '.env.local.sandbox'), join(process.cwd(), '.env.local'));
 
 // 本番環境を使用する場合は、以下のコメントを切り替えてください
-// copyFileSync(join(process.cwd(), 'env', '.env.local.product'), join(process.cwd(), '.env.local'));
+copyFileSync(join(process.cwd(), 'env', '.env.local.product'), join(process.cwd(), '.env.local'));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
